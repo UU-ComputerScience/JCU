7d3888c0a9a764c594bc61f964d0a705e25c0996
Alessandro Vermeulen <github@spockz.nl>
2012-11-19 12:43:58 +0100

    Added storage event handler wrapping.

--- resources/static/hjs/Data/LocalStorage.hs
+++ resources/static/hjs/Data/LocalStorage.hs
@@ -5,12 +5,18 @@ module Data.LocalStorage
   , clearLocalStorage
   , lengthLocalStorage
   , keyLocalStorage
+  , addStorageListener
   , StorageEvent(..)
   ) where
 
+import Language.UHC.JS.Primitives
 import Language.UHC.JS.Types
+
   
 import Language.UHC.JS.ECMA.String as JSString
+
+data JStorageEventPtr
+type JStorageEvent = JSPtr JStorageEventPtr
   
 data StorageEvent a = StorageEvent { key      :: String
                                    , oldValue :: a
@@ -21,7 +27,7 @@ foreign import js "localStorage.getItem(%1)"
   _getLocalStorage :: JSString -> IO a
     
 getLocalStorage :: Read a => String -> IO a
-getLocalStorage = fmap read . _getLocalStorage . toJS
+getLocalStorage = fmap (read . (fromJS :: JSString -> String)) . _getLocalStorage . toJS
 
 foreign import js "localStorage.setItem(%1, %2)"
   _setLocalStorage :: JSString -> a -> IO ()
@@ -46,3 +52,24 @@ foreign import js "localStorage.key(%1)"
 
 keyLocalStorage :: Int -> IO String
 keyLocalStorage = fmap fromJS . _keyLocalStorage
+
+foreign import js "window.addEventListener('storage', %1, false)"
+  _addStorageListener :: JSFunPtr (JStorageEvent -> IO ()) -> IO ()
+
+foreign import js "wrapper"
+  mkJStorageEventHandler :: (JStorageEvent -> IO ()) -> IO (JSFunPtr (JStorageEvent -> IO ()))
+  
+addStorageListener :: Read a => (StorageEvent a -> IO ()) -> IO ()
+addStorageListener f =
+  let g :: JStorageEvent -> IO ()
+      g obj = do storageevent <- convert obj
+                 f storageevent
+  in do eh <- mkJStorageEventHandler g
+        _addStorageListener eh        
+  where
+    convert :: Read a => JStorageEvent -> IO (StorageEvent a)
+    convert obj = do key <- fmap (fromJS :: JSString -> String)          $ getAttr "key" obj
+                     old <- fmap (read . (fromJS :: JSString -> String)) $ getAttr "old" obj
+                     new <- fmap (read . (fromJS :: JSString -> String)) $ getAttr "new" obj
+                     url <- fmap (fromJS :: JSString -> String)          $ getAttr "url" obj
+                     return $ StorageEvent key old new url
